a1278fee5a28a4d57852ee87e91272e17b752144
Ankit Jailwal <jailwalankit@gmail.com>
2022-01-28 07:53:07 +0530

    Fix in character neck movement

--- VrWithBoys/Assets/Scripts/NeckRotation.cs
+++ VrWithBoys/Assets/Scripts/NeckRotation.cs
@@ -7,19 +7,17 @@ public class NeckRotation : MonoBehaviour
     public Quaternion startQuaternioin;
     public Vector3 quaternionToVector;
     public Vector3 MoveVector;
-    // Start is called before the first frame update
+    public float lerpTime = 40;
     void Start()
     {
-        startQuaternioin = transform.rotation;     
+         
     }
     
     void Update()
     {
         Debug.Log("Forward " + Camera.main.transform.forward);
         Debug.Log("Rotation: " + Camera.main.transform.rotation);
-        Debug.Log("Angle: " + Camera.main.velocity);
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(Camera.main.transform.eulerAngles),Time.deltaTime*Camera.main.velocity[0]);
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(Camera.main.transform.eulerAngles),Time.deltaTime*Camera.main.velocity[1]);
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(Camera.main.transform.eulerAngles),Time.deltaTime*Camera.main.velocity[2]);
+        Debug.Log("Angle: " + Camera.main.transform.eulerAngles);
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(Camera.main.transform.eulerAngles),Time.deltaTime*lerpTime);
     }
 }
