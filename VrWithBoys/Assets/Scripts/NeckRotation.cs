536934362fa575508e15f7477826a4dda6611e64
Ankit Jailwal <jailwalankit@gmail.com>
2022-01-28 18:25:52 +0530

    Removed boilerplate code from NeckRotation.cs

--- VrWithBoys/Assets/Scripts/NeckRotation.cs
+++ VrWithBoys/Assets/Scripts/NeckRotation.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class NeckRotation : MonoBehaviour
 {
-    public Quaternion startQuaternioin;
-    public Vector3 quaternionToVector;
-    public Vector3 MoveVector;
     public float lerpTime = 40;
     void Start()
     {
@@ -15,9 +12,6 @@ public class NeckRotation : MonoBehaviour
     
     void Update()
     {
-        Debug.Log("Forward " + Camera.main.transform.forward);
-        Debug.Log("Rotation: " + Camera.main.transform.rotation);
-        Debug.Log("Angle: " + Camera.main.transform.eulerAngles);
         transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(Camera.main.transform.eulerAngles),Time.deltaTime*lerpTime);
     }
 }
