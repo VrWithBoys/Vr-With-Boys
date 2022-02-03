9242cdf8bfe9c7800d1abd8deb3ef87ddd2c59a5
Ankit Jailwal <jailwalankit@gmail.com>
2022-02-04 01:35:35 +0530

    Added Mirror package

---
+++ VrWithBoys/Assets/Scripts/NeckRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeckRotation : MonoBehaviour
+{
+    public float lerpTime = 40;
+  
+    void Start()
+    {
+         
+    }
+    
+    void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(Camera.main.transform.eulerAngles),Time.deltaTime*lerpTime);
+    }
+}
